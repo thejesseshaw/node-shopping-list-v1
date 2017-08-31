3901b1d894e792ea20e2dd0a936a780d4fd91d58
Jesse Shaw <jdshaw1987@gmail.com>
2017-08-31 12:10:54 -0400

    Completed Exercies for GET requests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +19,18 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
+
 app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+	res.json(ShoppingList.get());
+});
+
+
+app.get('/Recipes', (req, res) => {
+	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
